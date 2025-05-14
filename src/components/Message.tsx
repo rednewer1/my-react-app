9502746bd1e44223e0f4193ec47b9a5439ea9f7d
rednewer1 <rew1ndertimer@gmail.com>
2025-05-14 15:56:42 +0000

    14.05

---
+++ src/components/Message.tsx
@@ -0,0 +1,12 @@
+// src/components/Message.tsx
+import React from 'react';
+
+type MessageProps = {
+  text: string;
+};
+
+const Message: React.FC<MessageProps> = ({ text }) => {
+  return <div>{text}</div>;
+};
+
+export default Message;
